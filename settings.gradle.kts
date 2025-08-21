8627db894c55442d4ecedf21c3974d650481529e
velaphimathebula <velly.mathebula28@gmail.com>
2025-08-21 18:36:15 +0200

    files

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pokemons"
+include(":app")
+include(":homemodule")
+include(":infomodule")
+include(":core")
+include(":network")
